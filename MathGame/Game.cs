972c942a422f8a0472a6bc9aea2b6f81c478afd9
Marcin-Kumar <marcinkumar@gmail.com>
2025-04-26 16:17:13 +0200

    Split code from Main to Game class

---
+++ MathGame/Game.cs
@@ -0,0 +1,74 @@
+﻿namespace MathGame;
+
+internal class Game
+{
+    private const char AdditionCharacter = '+';
+    private const char DivisionCharacter = '/';
+    private const char MultiplicationCharacter = '*';
+    private const char SubtractionCharacter = '-';
+    private const char HistoryCharacter = 'h';
+    private const char ExitCharacter = 'e';
+    private int operationResult;
+    private int playerGuess;
+    private int firstValue;
+    private int secondValue;
+    public char gameOperator;
+
+    internal void DisplayMenu()
+    {
+        Console.WriteLine("You can test your arithmetic skills using the 4 basic artihmetic operations");
+        Console.WriteLine("Please find the possible operations you can choose.\n");
+        Console.WriteLine("\t* for Multiplication");
+        Console.WriteLine("\t/ for Division");
+        Console.WriteLine("\t- for Subtraction");
+        Console.WriteLine("\t+ for Addition");
+        Console.WriteLine("\tH or h to view game history\n");
+        Console.WriteLine("\tE or e to exit game\n");
+        Console.WriteLine("Please enter an option to start the game\n");
+    }
+
+    internal void GenerateRandomValues()
+    {
+        Random randomValueGenerator = new Random();
+        firstValue = randomValueGenerator.Next(101);
+        secondValue = randomValueGenerator.Next(101);
+    }
+
+    internal bool IsDivision() => gameOperator == DivisionCharacter;
+
+    internal bool IsDivisionResultInteger() => firstValue % secondValue == 0;
+
+    internal void DisplayOperation()
+    {
+        Console.WriteLine("\nThe input should be a valid whole number");
+        Console.WriteLine($"Guess the answer {firstValue} {gameOperator} {secondValue} = ");
+    }
+
+    internal void ExecuteGame()
+    {
+        bool isPlayerGuessInteger;
+        do
+        {
+            DisplayOperation();
+            isPlayerGuessInteger = int.TryParse(Console.ReadLine(), out playerGuess);
+        } while (!isPlayerGuessInteger);
+        operationResult = ExecuteOperation();
+    }
+
+    internal int ExecuteOperation() => gameOperator switch
+    {
+        MultiplicationCharacter => firstValue * secondValue,
+        DivisionCharacter => firstValue / secondValue,
+        AdditionCharacter => firstValue + secondValue,
+        SubtractionCharacter => firstValue - secondValue,
+        _ => throw new NotImplementedException()
+    };
+
+    internal bool IsEnteredOptionABasicArithmenticOperation() => gameOperator.Equals(MultiplicationCharacter) || gameOperator.Equals(AdditionCharacter) || gameOperator.Equals(SubtractionCharacter) || gameOperator.Equals(DivisionCharacter);
+
+    internal bool IsEnteredOptionForHistory() => char.ToLower(gameOperator).Equals(char.ToLower(HistoryCharacter));
+
+    internal bool IsEnteredOptionToExitGame() => char.ToLower(gameOperator).Equals(char.ToLower(ExitCharacter));
+
+    internal bool IsPlayerGuessAccurate() => playerGuess == operationResult;
+}
--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -2,17 +2,7 @@
 
 internal class Program
 {
-    private const char AdditionCharacter = '+';
-    private const char DivisionCharacter = '/';
-    private const char HistoryCharacter = 'h';
-    private const char MultiplicationCharacter = '*';
-    private const char SubtractionCharacter = '-';
-    private const char ExitCharacter = 'e';
     private static List<string> s_gameRecord = new List<string>();
-    private static int s_operationResult;
-    private static int s_playerGuess;
-    private static int s_firstValue;
-    private static int s_secondValue;
 
     private static void Main(string[] args)
     {
@@ -21,26 +11,28 @@ internal class Program
         
         do
         {
-            DisplayMenu();
-            char menuOption = Console.ReadKey().KeyChar;
-            if (IsEnteredOptionABasicArithmenticOperation(menuOption))
-            {
-                GenerateRandomValues();
+            Game game = new Game();
 
-                if (menuOption == DivisionCharacter)
+            game.DisplayMenu();
+            game.gameOperator = Console.ReadKey().KeyChar;
+            if (game.IsEnteredOptionABasicArithmenticOperation())
+            {
+                game.GenerateRandomValues();
+
+                if (game.IsDivision())
                 {
-                    while (!IsDivisionResultInteger())
+                    while (!game.IsDivisionResultInteger())
                     {
-                        GenerateRandomValues();
+                        game.GenerateRandomValues();
                     }
                 }
 
-                ExecuteGame(menuOption);
-                string gameResult = IsPlayerGuessAccurate() ? "Correct" : "Wrong";
+                game.ExecuteGame();
+                string gameResult = game.IsPlayerGuessAccurate() ? "Correct" : "Wrong";
                 s_gameRecord.Add(gameResult);
                 Console.WriteLine($"{gameResult}\n");
             }
-            else if (IsEnteredOptionForHistory(menuOption))
+            else if (game.IsEnteredOptionForHistory())
             {
                 if (s_gameRecord.Count == 0)
                 {
@@ -56,7 +48,7 @@ internal class Program
                 }
                 Thread.Sleep(1800);
             }
-            else if (IsEnteredOptionToExitGame(menuOption)) { 
+            else if (game.IsEnteredOptionToExitGame()) { 
                 continueGame = false;
             }
             else
@@ -68,60 +60,4 @@ internal class Program
             }
         } while (continueGame);
     }
-
-    private static void DisplayMenu()
-    {
-        Console.WriteLine("You can test your arithmetic skills using the 4 basic artihmetic operations");
-        Console.WriteLine("Please find the possible operations you can choose.\n");
-        Console.WriteLine("\t* for Multiplication");
-        Console.WriteLine("\t/ for Division");
-        Console.WriteLine("\t- for Subtraction");
-        Console.WriteLine("\t+ for Addition");
-        Console.WriteLine("\tH or h to view game history\n");
-        Console.WriteLine("\tE or e to exit game\n");
-        Console.WriteLine("Please enter an option to start the game\n");
-    }
-
-    private static void GenerateRandomValues()
-    {
-        Random randomValueGenerator = new Random();
-        s_firstValue = randomValueGenerator.Next(101);
-        s_secondValue = randomValueGenerator.Next(101);
-    }
-
-    private static bool IsDivisionResultInteger() => s_firstValue % s_secondValue == 0;
-
-    private static void DisplayOperation(char operation)
-    {
-        Console.WriteLine("\nThe input should be a valid whole number");
-        Console.WriteLine($"Guess the answer {s_firstValue} {operation} {s_secondValue} = ");
-    }
-
-    private static void ExecuteGame(char menuOption)
-    {
-        bool isPlayerGuessInteger;
-        do
-        {
-            DisplayOperation(menuOption);
-            isPlayerGuessInteger = int.TryParse(Console.ReadLine(), out s_playerGuess);
-        } while (!isPlayerGuessInteger);
-        s_operationResult = ExecuteOperation(menuOption);
-    }
-
-    private static int ExecuteOperation(char operation) => operation switch
-    {
-        MultiplicationCharacter => s_firstValue * s_secondValue,
-        DivisionCharacter => s_firstValue / s_secondValue,
-        AdditionCharacter => s_firstValue + s_secondValue,
-        SubtractionCharacter => s_firstValue - s_secondValue,
-        _ => throw new NotImplementedException()
-    };
-
-    private static bool IsEnteredOptionABasicArithmenticOperation(char menuOption) => menuOption.Equals(MultiplicationCharacter) || menuOption.Equals(AdditionCharacter) || menuOption.Equals(SubtractionCharacter) || menuOption.Equals(DivisionCharacter);
-
-    private static bool IsEnteredOptionForHistory(char menuOption) => char.ToLower(menuOption).Equals(char.ToLower(HistoryCharacter));
-    private static bool IsEnteredOptionToExitGame(char menuOption) => char.ToLower(menuOption).Equals(char.ToLower(ExitCharacter));
-
-    private static bool IsPlayerGuessAccurate() => s_playerGuess == s_operationResult;
-
 }
